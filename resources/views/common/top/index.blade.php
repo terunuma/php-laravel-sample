eb79aa85cdc7c3032f367e330676e4362d6768f3
terunuma <terunuma@users.noreply.github.com>
2018-02-25 16:37:32 +0900

    add bg-main color

--- resources/views/common/top/index.blade.php
+++ resources/views/common/top/index.blade.php
@@ -1,9 +1,11 @@
-<div class="l-wrapper">
-    トップページ
-    <br>
-    @auth
-        <span>ログインしてる</span>
-    @else
-        <span>ログインしてない</span>
-    @endauth
+<div class="l-page-wrapper">
+    <div class="l-wrapper">
+        トップページ
+        <br>
+        @auth
+            <span>ログインしてる</span>
+        @else
+            <span>ログインしてない</span>
+        @endauth
+    </div>
 </div>
