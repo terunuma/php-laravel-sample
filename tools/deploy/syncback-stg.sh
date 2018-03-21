7ce4102aac67f4e4a0a1339665cc4f519586380c
cdox <cadox@stg.web-x.co.jp>
2018-03-21 15:08:02 +0900

    add syncback command

--- tools/deploy/syncback-stg.sh
+++ tools/deploy/syncback-stg.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-function deploy()
+function sync_back()
 {
-    local owner=www-data
+    local owner=cadox
     local script_dir=$(cd $(dirname $0); pwd)
     local target_dir=${script_dir}/../../
     local base_dir=/home/sdl/devel/sidebizz/
     rsync -azv \
         --exclude '.git' \
-        ${target_dir} ${base_dir}
+        ${base_dir} ${target_dir}
 
     echo "chown files..."
-    chown -R ${owner}:${owner} ${base_dir}
+    chown -R ${owner}:${owner} ${target_dir}
 }
 
-deploy
+sync_back
 
